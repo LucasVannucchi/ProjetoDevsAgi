fb4107364aee2a60bfdce42d92f15f32bbc89bb6
dev11 <lucasvannucchi16@gmail.com>
2025-08-13 11:23:29 -0300

    primeira/Alteracao

--- src/br/com/devsdoagi/JavaBasico/Matriz/Exercicios/Exercicio03.java
+++ src/br/com/devsdoagi/JavaBasico/Matriz/Exercicios/Exercicio03.java
@@ -3,11 +3,16 @@ package br.com.devsdoagi.JavaBasico.Matriz.Exercicios;
 public class Exercicio03 {
     public static double[] encontrarMenorPrecoPorProduto(double[][] matriz){
         double menor;
+        double[] menorPreco = new double[matriz[0].length];
         for (int p = 0; p < matriz[0].length; p++) {
             menor = matriz[0][p];
             for (int l = 0; l < matriz.length; l++) {
-
+                if (matriz[l][p] < menor){
+                    menor = matriz[l][p];
+                }
             }
+            menorPreco[p] = menor;
         }
+        return menorPreco;
     }
 }
